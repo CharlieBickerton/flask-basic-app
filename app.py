18630bdbbdf945c02c4e578f00543a662ec27e0c
Charlie Bickerton <charlie@mimin.al>
2019-06-07 10:32:09 +0100

    using templates for each page. Passing in mock data and accessing data in html

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+# dummy data
+posts = [
+    {
+        'author': 'Charlie Bickerton',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'Janurary 7, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'February 7, 2019'
+    },
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1> Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1> About Page</h1>"
+    return render_template('about.html')
 
 
 # this will allow us to run the app with python
