2c4bebba91f748169349ab6ad3daea48fd0cf0fd
Charlie Bickerton <charlie@mimin.al>
2019-06-07 16:57:42 +0100

    created an sql db and using flask-sqlalchemy to create tables of linked users and posts

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'b182309d4ff36f9a272cc3495da8475b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-# dummy data
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    # the image file is going to be hashed so it is 20 characters long
+    password = db.Column(db.String(60), nullable=False)
+    # the password will also be hashed but to 60
+    posts = db.relationship('Post', backref='author', lazy=True)
+    # this posts relationship is not a column but an addtional query that runs in the back
+    # ground to collect all the posts from a user
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # this user_id is a column but we are letting the db know that it is a key from a
+    # another table in the db from the user model
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
 posts = [
     {
         'author': 'Charlie Bickerton',
