5fbedddbccf1ba5dc16f7ec4e8ee242b61f2a92a
Charlie Bickerton <charlie@mimin.al>
2019-06-10 16:13:13 +0100

    added in email token functionality and .env file for the secrets to enable a user to request a password change

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b182309d4ff36f9a272cc3495da8475b'
@@ -12,5 +17,11 @@ login_manager = LoginManager(app)
 # tell the log in manager what the login route is (for the redirect on not logged in users)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
+mail = Mail(app)
 
 from app import routes
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from app import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serialiser
+from app import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -11,13 +12,28 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False,
-                           default='default.jpg')
+                            default='default.jpg')
     # the image file is going to be hashed so it is 20 characters long
     password = db.Column(db.String(60), nullable=False)
     # the password will also be hashed but to 60
     posts = db.relationship('Post', backref='author', lazy=True)
     # this posts relationship is not a column but an addtional query that runs in the back
     # ground to collect all the posts from a user
+
+    def get_reset_token(self, expires_sec=1800): # 30 mins
+        '''returns a token in utf-8'''
+        s = Serialiser(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        '''Checks the token is still live - if not error it returns the user'''
+        s = Serialiser(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
