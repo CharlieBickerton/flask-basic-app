ffffd0a59b5091b5c8d327cca01b44d55fa8f556
Charlie Bickerton <charlie@mimin.al>
2019-06-11 10:57:28 +0100

    added a config file and didnt quite catch all of the url_for changes when moving over to blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,20 @@
-import os
-from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from app.config import Config
 
-load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'b182309d4ff36f9a272cc3495da8475b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 # tell the log in manager what the login route is (for the redirect on not logged in users)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
+
 mail = Mail(app)
 
 from app.users.routes import users
